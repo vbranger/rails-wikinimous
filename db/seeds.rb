dc9b1493f370ba801c7758455cc0a2a14e9c094b
Victor Branger <victor.branger@gmail.com>
2020-11-10 14:42:47 +0100

    model article and seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating articles"
+
+10.times do
+  article = Article.new(
+    title: Faker::Movie.title,
+    content: Faker::Movie.quote,
+  )
+  article.save!
+end
+
+puts "finished"
